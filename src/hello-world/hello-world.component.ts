707991762640ea6f469c70e90e58e07237c422c5
Fabian Wiles <fabianwiles@live.com>
2018-02-17 15:25:13 +1300

    a

--- src/hello-world/hello-world.component.ts
+++ src/hello-world/hello-world.component.ts
@@ -1,43 +1,12 @@
 import {Component, NgModule} from '@angular/core';
 import {msg} from '../lib/file';
-
+// some comment
 @Component({
   selector: 'hello-world-app',
   template: `
     <div>Hello {{ name }}</div>
     <input type="text" [value]="name" (input)="name = $event.target.value"/>
     hhheellllooo
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
   `,
   styleUrls: ['./hello-world-styles.css']
 })
