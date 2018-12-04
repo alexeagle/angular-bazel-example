a55f5064096b0749a11de1132feeaa195638d0a2
Miles Matthias <mmatthias@google.com>
2018-12-04 14:46:01 -0700

    wip on todos section

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,13 +8,14 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {MaterialModule} from './material/material.module';
 import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
 import {todoReducer} from './reducers/reducers';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     AppRoutingModule, BrowserModule, BrowserAnimationsModule, MaterialModule,
-    MatCardModule, StoreModule.forRoot({todoReducer})
+    MatListModule, MatCardModule, StoreModule.forRoot({todoReducer})
   ],
   exports: [AppComponent],
   bootstrap: [AppComponent],
